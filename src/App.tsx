05a307a1d4fe669205f9154c1bcc7f17a81bcbf3
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-08-07 02:26:50 +0330

    add row chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,103 @@
-import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
-import { PDFViewer } from "@react-pdf/renderer";
+import {
+  Document,
+  PDFViewer,
+  Page,
+  View,
+  Text,
+  Font,
+} from "@react-pdf/renderer";
+import vR from "./fonts/Vazirmatn-Regular.ttf";
+import vB from "./fonts/Vazirmatn-Bold.ttf";
 import classNames from "./App.module.css";
+import { mock } from "./mock";
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
+// TODO: take these in as props
+// const fieldsToIgnore = ["deleted_at", "_count", "id"];
+
+Font.register({
+  family: "Vazirmatn-Regular",
+  src: vR,
 });
 
-// Create Document Component
-const MyDocument = () => (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text>Section #1</Text>
-        </View>
-        <View style={styles.section}>
-          <Text>Section #2</Text>
-        </View>
-      </Page>
+Font.register({
+  family: "Vazirmatn-Bold",
+  src: vB,
+});
+
+const _MAX_ROWS_PER_PAGE = 8;
+
+const getRowChunks = (
+  rows: object[],
+  MAX_ROWS_PER_PAGE = _MAX_ROWS_PER_PAGE
+) => {
+  const chunk = [];
+  for (let i = 0; i < rows.length; i += MAX_ROWS_PER_PAGE) {
+    chunk.push(rows.slice(i, i + MAX_ROWS_PER_PAGE));
+  }
+  return chunk;
+};
+
+const DatasetTableDocument = ({
+  data,
+  maxRowsPerPage,
+}: {
+  data: object[];
+  maxRowsPerPage?: number;
+}) => {
+
+  // TODO: need column chunks too. calculate pages based on col chunks and row chunks.
+  const rowChunks = getRowChunks(data, maxRowsPerPage);
+
+  return (
+    <Document style={{ fontFamily: "Vazirmatn-Regular", fontSize: 12 }}>
+      {rowChunks.map((rowChunk, i) => {
+        return (
+          <Page
+            size="A4"
+            orientation="landscape"
+            key={i}
+            style={{ padding: 16 }}
+          >
+            <View style={{ border: 1, borderBottom: 0 }}>
+              {rowChunk.map((row) => (
+                <Row rowData={row} />
+              ))}
+            </View>
+          </Page>
+        );
+      })}
     </Document>
-);
+  );
+};
+
+function Row({ rowData }: { rowData: object }) {
+  return (
+    <View style={{ display: "flex", flexDirection: "row-reverse" }}>
+      {Object.values(rowData).map((val) => (
+        <View
+          style={{
+            overflow: "hidden",
+            padding: 4,
+            width: 100,
+            height: 30,
+            border: 1,
+          }}
+        >
+          <Text style={{ backgroundColor: "blue" }}>
+            {typeof val === "string" || typeof val === "number"
+              ? val
+              : "not allowed"}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
+}
 
 function App() {
   return (
     <PDFViewer className={classNames.container}>
-      <MyDocument />
+      <DatasetTableDocument data={mock} />
     </PDFViewer>
   );
 }
