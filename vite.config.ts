680cc1c38389a4a1443431adde974f53be510fa7
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-08-12 20:08:01 +0330

    publish v0.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [dts({ exclude: ["node_modules/**", "src/demo/**"] }), react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/mym-dataset-pdf.tsx"),
+      // the proper extensions will be added
+      name: "MyMDatasetPdf",
+      fileName: "mym-dataset-pdf",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom", "@react-pdf/renderer"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
