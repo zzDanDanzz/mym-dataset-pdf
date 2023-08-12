90442eff0dca32ff6e44030e3cebb7e2f2738031
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-08-12 22:51:50 +0330

    receive title as prop

--- src/mym-dataset-pdf.tsx
+++ src/mym-dataset-pdf.tsx
@@ -4,22 +4,25 @@ import Table from "./components/table";
 import _ from "./components/utils/lodash";
 import FontContext, { IFontContext } from "./context/fontFamilies";
 
-const _FIELDS_TO_IGNORE = ["id", "_count", "deleted_at"];
-const _MAX_COLS_PER_PAGE = 5;
-const _MAX_ROWS_PER_PAGE = 8;
+const _DEFAULT_FIELDS_TO_IGNORE = ["id", "_count", "deleted_at"];
+const _DEFAULT_MAX_COLS_PER_PAGE = 5;
+const _DEFAULT_MAX_ROWS_PER_PAGE = 8;
+const _DEFAULT_TITLE = "بدون نام";
 
 interface IDatasetTableDocument {
   data: object[];
   maxRowsPerPage?: number;
   maxColsPerPage?: number;
   fieldsToIgnore?: string[];
+  title?: string;
 }
 
 const DatasetTableDocument = ({
   data,
-  maxRowsPerPage = _MAX_ROWS_PER_PAGE,
-  maxColsPerPage = _MAX_COLS_PER_PAGE,
-  fieldsToIgnore = _FIELDS_TO_IGNORE,
+  maxRowsPerPage = _DEFAULT_MAX_ROWS_PER_PAGE,
+  maxColsPerPage = _DEFAULT_MAX_COLS_PER_PAGE,
+  fieldsToIgnore = _DEFAULT_FIELDS_TO_IGNORE,
+  title = _DEFAULT_TITLE,
 }: IDatasetTableDocument) => {
   const fontFamilies = useContext(FontContext);
 
@@ -37,6 +40,7 @@ const DatasetTableDocument = ({
         fontSize: 12,
         ...(fontFamilies.regular && { fontFamily: fontFamilies.regular }),
       }}
+      title={title}
     >
       {colsNamesChunks.map((colNames, ic) => {
         return tableDataChunks.map((tableData, it) => (
